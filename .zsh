0a0e0e735547005310f9081454af13975b9be3bd
Cole Gentry <Peapod2007@gmail.com>
2019-07-09 20:22:27 -0400

    Initial commit
    Signed-off-by: Cole Gentry <Peapod2007@gmail.com>

---
+++ .zsh
@@ -0,0 +1,75 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/Users/cole/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+# stop these errors https://github.com/asdf-vm/asdf/issues/266
+# & make it fast https://carlosbecker.com/posts/speeding-up-zsh/
+autoload -Uz compinit
+
+# My Custom Aliases
+cd ~/Documents/proj
+alias proj='cd ~/Documents/proj'
+alias lp="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+if [ $(date +'%j') != $(stat -f '%Sm' -t '%j' ~/.zcompdump) ]; then
+    compinit;
+else
+    compinit -C;
+fi;
+
+##########################################################
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
+
+# Make sure to use double quotes #########################
+zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-autosuggestions"
+#zsh-completions
+fpath=(/usr/local/share/zsh-completions $fpath)
+
+# oh-my-zsh ##############################################
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/github", from:oh-my-zsh
+zplug "plugins/command-not-found", from:oh-my-zsh
+zplug "plugins/autojump", from:oh-my-zsh
+zplug "plugins/compleat", from:oh-my-zsh
+zplug "plugins/ssh-agent", from:oh-my-zsh
+# Node Plugins
+zplug "plugins/node", from:oh-my-zsh
+
+# Load if "if" tag returns true
+zplug "lib/clipboard", from:oh-my-zsh
+zplug "oz/safe-paste"
+
+# Note: To specify the order in which packages should be loaded, use the defer
+#       tag described in the next section
+
+# Set the priority when loading
+# e.g., zsh-syntax-highlighting must be loaded
+# after executing compinit command and sourcing other plugins
+# (If the defer tag is given 2 or above, run after compinit command)
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
+# Load theme file ########################################
+# zplug denysdovhan/spaceship-prompt, use:spaceship.zsh, from:github, as:theme
+zplug dikiaap/dotfiles, use:.oh-my-zsh/themes/oxide.zsh-theme, from:github, as:theme
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+
+zplug load
+
