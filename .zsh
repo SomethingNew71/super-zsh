3b65cb11feef4dc15b84e5abf6297753ccd3962f
Cole Gentry <peapod2007@gmail.com>
2020-08-04 10:26:00 -0400

    Adds proxy script reference and colorLS configs
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- .zsh
+++ .zsh
@@ -19,6 +19,9 @@ cd ~/Documents/proj
 alias proj='cd ~/Development'
 alias gs="git status"
 alias lp="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias lcc='colorls -lA --sd'
+alias lca='colorls -a --sd -sf'
+alias lct='colorls --tree=2'
 
 current_user=$USER
 node_version=$(node -v 2>/dev/null)
@@ -33,6 +36,8 @@ fi;
 ##########################################################
 export ZPLUG_HOME=/usr/local/opt/zplug
 source $ZPLUG_HOME/init.zsh
+# ColorLS plugin needs manual installing - https://github.com/athityakumar/colorls#custom-configurations
+source $(dirname $(gem which colorls))/tab_complete.sh
 
 # Make sure to use double quotes #########################
 zplug "zsh-users/zsh-history-substring-search"
@@ -77,4 +82,57 @@ if ! zplug check --verbose; then
 fi
 
 zplug load
+
+##############################
+# CORPORATE PROXY COMMANDS
+##############################
+PROXY_URL="ENTER YOUR DUMB PROXY HERE"
+
+proxy-on() {
+    echo "Enter your username:"
+    read USERNAME
+    echo "Please enter your password:"
+    read -s PASSWORD
+    PROXY="http://${USERNAME}:${PASSWORD}@${PROXY_URL}"
+    export https_proxy=${PROXY}
+    export HTTPS_PROXY=${PROXY}
+    export http_proxy=${PROXY}
+    export HTTP_PROXY=${PROXY}
+    export no_proxy=${PROXY}
+    export NO_PROXY=${PROXY}
+    git config --global http.proxy ${PROXY}
+    git config --global https.proxy ${PROXY}
+    git config --global http.sslVerify "false"
+    git config --global url."https://".insteadOf "git://"
+    npm config set ca=""
+    # npm config set registry "SPECIAL REGISTRY GOES HERE"
+    npm config set strict-ssl false
+    npm config set proxy ${PROXY}
+    npm config set https-proxy ${PROXY}
+    echo "======================="
+    echo "Proxy has been set - 🛰️"
+    echo "======================="
+}
+proxy-off() {
+    NO_PROXY="localhost,.bullshitcorp.net,jira,complaints,whistleblowerblower"
+    unset https_proxy
+    unset HTTPS_PROXY
+    unset http_proxy
+    unset HTTP_PROXY
+    unset no_proxy
+    unset NO_PROXY
+    export NODE_TLS_REJECT_UNAUTHORIZED=1
+    git config --global unset http.proxy
+    git config --global unset https.proxy
+    git config --global unset http.sslVerify "false"
+    git config --global unset url."https://".insteadOf "git://"
+    npm config delete ca=""
+    # npm config set registry "SPECIAL REGISTRY GOES HERE"
+    npm config set strict-ssl true
+    npm config delete proxy ${PROXY}
+    npm config delete https-proxy ${PROXY}
+    echo "========================="
+    echo "Proxy has been unset - ❌"
+    echo "========================="
+}
 
